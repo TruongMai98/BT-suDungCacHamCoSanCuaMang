8c9c50c4216b2b1ce2aefb0c5a6766718a652472
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-09-19 14:34:07 +0700

    update1

--- bt3.js
+++ bt3.js
@@ -3,10 +3,21 @@ let str = n.toString();
 let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lower = "abcdefghijklmnopqrstuvwxyz";
 let result = [];
-for (let i = 0; i < str.length; i++) {
+/*for (let i = 0; i < str.length; i++) {
     if (upper.indexOf(str[i]) !== -1) {
         result.push(str[i].toLowerCase());
     } else if (lower.indexOf(str[i]) !== -1) {
+        result.push(str[i].toUpperCase());
+    } else {
+        result.push(str[i]);
+    }
+}
+console.log(result.join(''));
+document.write(result.join(''));*/
+for (let i = 0; i < str.length; i++) {
+    if (upper.includes(str[i])) {
+        result.push(str[i].toLowerCase());
+    } else if (lower.includes(str[i])) {
         result.push(str[i].toUpperCase());
     } else {
         result.push(str[i]);
